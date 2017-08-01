27f9289dd2fdfac967f93e8de701235aa45421d3
Jorge Téllez <novohispano@gmail.com>
2017-08-01 13:12:28 -0600

    added error messages in form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_form_errors(object, attribute)
+    messages = object.errors.messages[attribute.to_sym].uniq
+    messages.map { |message| "#{attribute.capitalize} #{message}." }.join(' ')
+  end
 end
