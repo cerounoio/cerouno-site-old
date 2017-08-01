bfe28cb9fc460f2df6ddfd63e6eb27e1fb7ea07f
Jorge Téllez <novohispano@gmail.com>
2017-08-01 12:38:39 -0600

    changes users/new to register

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Sessions
   get  'login',  to: 'sessions#new'
   post 'login',  to: 'sessions#create'
   get  'logout', to: 'sessions#destroy'
 
-  resources 'users', except: [:index, :destroy]
+  # Users
+  get 'register', to: 'users#new'
+  resources 'users', except: [:index, :destroy, :new]
 
+  # Home
   root 'home#show'
 end
