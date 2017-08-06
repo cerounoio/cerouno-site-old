c8352c6406587753b7aaabfdf44d7055a9a10e95
Jorge Téllez <novohispano@gmail.com>
2017-08-05 18:10:19 -0600

    updates file input to match the name of the file

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,9 +1,16 @@
 $(document).on('ready turbolinks:load', function(){
   trackRegistration();
+  changeFileInputContent();
 });
 
 function trackRegistration() {
   $( '#registerButton' ).click(function() {
     fbq('track', 'Lead');
+  });
+}
+
+function changeFileInputContent() {
+  $('input[type="file"]').on('change', function() {
+    $(this).next('.custom-file-control').attr('data-content-value', $(this)[0].files[0].name);
   });
 }
