d2f683317c47505f1bab3a9b977b98a4b600ca66
Jorge Téllez <novohispano@gmail.com>
2017-10-16 15:08:24 -0500

    fixes hostname in production for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "cerouno-site_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://wwww.cerouno.io' }
+  config.action_mailer.default_url_options = { host: 'http://www.cerouno.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sparkpostmail.com',
