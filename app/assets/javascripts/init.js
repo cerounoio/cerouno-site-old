62c13712a57f6f62cc037af0b6b9de856d681fcb
Jorge Téllez <novohispano@gmail.com>
2018-04-16 16:03:13 -0500

    adds ability to copy emails

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -4,6 +4,7 @@ $(document).on('ready turbolinks:load', function(){
   changeTeachersDataOnClick();
   changeTeamDataOnClick();
   changeProgramMentorsDataOnClick();
+  copyApplicationEmails();
 });
 
 function trackRegistration() {
@@ -16,4 +17,31 @@ function changeFileInputContent() {
   $('input[type="file"]').on('change', function() {
     $(this).next('.custom-file-control').attr('data-content-value', $(this)[0].files[0].name);
   });
+}
+
+function copyApplicationEmails() {
+  $('#copyApplicationEmails').click(function() {
+    var applicantEmails = [];
+
+    applicantEmails     = getApplicantData()
+    applicantEmailsText = applicantEmails.get().join(', ')
+
+    prompt('Copia los emails de las aplicaciones con CMD+C', applicantEmailsText)
+  })
+}
+
+function getApplicantData() {
+  var firstName = ""
+  var lastName  = ""
+  var email     = ""
+  var applicant = ""
+
+  return $('.application').map(function() {
+    firstName = $(this).data('first-name')
+    lastName  = $(this).data('last-name')
+    email     = $(this).data('email')
+    applicant = `${firstName} ${lastName} <${email}>`
+
+    return applicant
+  })
 }
