10bbf9d12d7a5ddf431b248152c64dde5c0222ca
Jorge Téllez <novohispano@gmail.com>
2018-04-17 20:41:04 -0500

    fixes undefined method bug on intercom

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -63,7 +63,7 @@ IntercomRails.config do |config|
       last_name:            Proc.new { |user| user.try(:last_name) },
       email:                Proc.new { |user| user.try(:email) },
       whatsapp:             Proc.new { |user| user.try(:whatsapp) },
-      application_status:   Proc.new { |user| user.try(:application).try(status) },
+      application_status:   Proc.new { |user| user.try(:application).try(:status) },
       gender:               Proc.new { |user| user.application.try(:demographic).try(:gender) },
       objective:            Proc.new { |user| user.application.try(:experience).try(:objective) },
       technical_experience: Proc.new { |user| user.application.try(:experience).try(:technical_experience) },
