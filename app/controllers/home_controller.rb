1a56b837075e6307fe836894d935759f77d38b7e
Jorge Téllez <novohispano@gmail.com>
2018-04-26 17:03:40 -0500

    adds hyperlinks in pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
   def show
   end
 
-  def program
+  def terms
   end
 
-  def programs
+  def privacy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'terms',   to: 'home#terms'
+  get 'privacy', to: 'home#privacy'
+
   get  'login',  to: 'sessions#new'
   post 'login',  to: 'sessions#create'
   get  'logout', to: 'sessions#destroy'
